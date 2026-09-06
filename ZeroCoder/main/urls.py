c40e37e6ff628981caca282a0e88ee358efd1c23
AnyKey <andyklepaki@yandex.ru>
2026-09-06 16:04:13 +0300

    22

--- ZeroCoder/main/urls.py
+++ ZeroCoder/main/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
-    path('new', views.new),
+    path('', views.index, name='home'),
+    path('new', views.new, name='new'),
     path('data', views.data),
     path('test', views.test)
 ]
