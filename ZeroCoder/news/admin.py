95ed544bcce3742d6320314e1e4d189392c4d230
AnyKey <andyklepaki@yandex.ru>
2026-09-10 12:19:25 +0300

    dz2

--- ZeroCoder/news/admin.py
+++ ZeroCoder/news/admin.py
@@ -1,4 +1,21 @@
 from django.contrib import admin
-
 from .models import News_post
-admin.site.register(News_post)
+
+
+@admin.register(News_post)
+class NewsPostAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'author',
+        'pub_date',
+    )
+
+    search_fields = (
+        'title',
+        'short_description',
+        'author',
+    )
+
+    list_filter = (
+        'pub_date',
+    )
